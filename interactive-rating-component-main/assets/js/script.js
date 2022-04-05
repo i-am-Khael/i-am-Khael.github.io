21e4dfbdc0442a95191df9fd20bdc7d94b20b19c
i-am-Khael <casalsmichael@protonmail.com>
2022-04-05 17:15:29 +0800

    refactored script on interactive-rating-component

--- interactive-rating-component-main/assets/js/script.js
+++ interactive-rating-component-main/assets/js/script.js
@@ -5,25 +5,34 @@ const btnSubmit = document.querySelector('.btnSubmit');
 const interactiveRating = document.querySelector('section.interactiveRating');
 const container = document.querySelector('div.container');
 
+const removeActiveClass = () => {
+
+  btns.forEach(btn => {
+
+    btn.classList.remove('active');
+
+  });
+
+}
 
 btns.forEach((btn, index) => {
   
-  btn.addEventListener('click', () => {
+  const rate = index + 1;
+  
+  btn.addEventListener('click', (e) => {
     
-    const rate = index + 1;
+    removeActiveClass();
 
-    btn.style.backgroundColor = 'hsl(217, 12%, 63%)';
-    btn.style.color = 'var(--neutral-white)';
-    
+    e.currentTarget.classList.add('active');
+
     btnSubmit.addEventListener('click', () => {
 
+      rating.textContent = `You selected ${rate} out of 5`;
       interactiveRating.style = 'display: none';
       container.style = 'display: block';
 
-      rating.textContent = `You selected ${rate} out of 5`;
-
-    })
+    });
 
   });
-    
+
 });
